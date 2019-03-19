a5d1d8ddab60b65814d729319ee578920900a712
unknown <asiamcarter@gmail.com>
2019-03-19 17:27:27 -0500

    setting up program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace StudentExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository repository = new Repository();
+
+           
+                        
+        }
+
+        public static void Pause()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercise3
+{
+    class Repository
+    {
+        //Query the database for all the Exercises.
+
+    }
+}
